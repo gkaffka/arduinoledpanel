b4937ca25bc2d24e6b63f18821c255b452c55452
Gabriel Kaffka <kaffka@gmail.com>
2017-03-06 23:52:17 -0300

    Adds fill code and bluetooth connection status indicator

--- led_matrix_256_bluetooth/led_matrix_256_bluetooth.ino
+++ led_matrix_256_bluetooth/led_matrix_256_bluetooth.ino
@@ -4,7 +4,7 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTserial(2, 3); // RX | TX
 CRGB leds[NUM_LEDS];
-
+boolean fill=false;
 int i=0;
 void setup() {
  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
@@ -18,6 +18,7 @@ void loop() {
         char c = BTserial.read();
         if(i<15){
             if(c=='@') clearScreen();
+            else if(c=='$') fill = true;
             else{
                 received[i] = c;
                 Serial.println(i);
@@ -29,6 +30,14 @@ void loop() {
     i=0;
     
 }
+
+void fillScreen(int r, int g, int b){
+  for(int i=0;i<NUM_LEDS;i++){
+    leds[i] = CRGB(r, g, b);
+    }
+    FastLED.show();
+  }
+
 void clearScreen(){
   for(int i=0;i<NUM_LEDS;i++){
     leds[i] = CRGB(0, 0, 0);
@@ -77,6 +86,11 @@ void parseValues(char velues[]){
  r[3] = '\0';
  g[3] = '\0';
  b[3] = '\0';
- drawPixel(atoi(p),atoi(r),atoi(g),atoi(b));
+ if(fill){
+    fillScreen(atoi(r),atoi(g),atoi(b));
+    fill = false;
+    }
+ else
+    drawPixel(atoi(p),atoi(r),atoi(g),atoi(b));
 }
 
